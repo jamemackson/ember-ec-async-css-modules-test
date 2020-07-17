24b8f598a493435e8a7af007d0fd63b25f288b0b
jame mackson <jamemackson@icloud.com>
2020-07-17 11:18:42 -0500

    add a basic ec task

--- app/components/test-thing.js
+++ app/components/test-thing.js
@@ -1,4 +1,16 @@
-import Component from '@glimmer/component';
-
+import Component from "@glimmer/component";
+import { task } from "ember-concurrency-decorators";
+import { timeout } from "ember-concurrency";
+import { tracked } from "@glimmer/tracking";
 export default class TestThingComponent extends Component {
+  @tracked status = "initialized";
+  @task *waitAFewSeconds() {
+    this.status = "Gimme one second...";
+    yield timeout(1000);
+    this.status = "Gimme one more second...";
+    yield timeout(1000);
+    this.status = "OK, I'm done.";
+    yield timeout(2000);
+    this.status = "initialized";
+  }
 }
